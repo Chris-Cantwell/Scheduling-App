5d96a2ed9515d944aaef15cdc334bd806b99d6b2
Chris Catwell <christophercantwell@college.harvard.edu>
2023-03-06 03:24:07 -0500

    Click and Drag v1

--- src/routes/scheduling/+page.svelte
+++ src/routes/scheduling/+page.svelte
@@ -1,32 +1,18 @@
 <script>
     import Table from './Table.svelte';
-    let test = 0;
-
-    function handleClick(event) {
-        console.log("User clicked");
-        console.log(event);
-    }
-
-    let mousepos = { x: 0, y: 0 }
-
-    function handleMouseMove(event) {
-        console.log("Mouse Moving Through Div!");
-        mousepos.x = event.clientX;
-        mousepos.y = event.clientY;
-    }
 
     let week = {
-        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        // Add a blank precursor day to hold time block labels
+        days: ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
         earliest_time: {hour: 8, minute: 0}, // Time code, 24h hh:mm
         latest_time: {hour: 18, minute: 0}, // Time code, 24h hh:mm
-        interval_min: 15 // Interval in Minutes
+        interval_minutes: 30 // Interval in Minutes
     }
 
 </script>
 
 <center>
     <h1> Please enter your availability! </h1>
-    <div class="grid-item-test" on:mousedown={handleMouseMove}>Test -- Mouse is at {mousepos.x}, {mousepos.y}</div>
 </center>
 
 <Table week={week}/> 
--- src/routes/scheduling/Table.svelte
+++ src/routes/scheduling/Table.svelte
@@ -1,44 +1,99 @@
 <script>
+   import { each } from "svelte/internal";
+
    export let week;
    let days = week.days;
 
-   let num_intervals = (
-    ((week.latest_time.hour * 60 + week.latest_time.minute) - 
-    (week.earliest_time.hour * 60 + week.earliest_time.minute)) / week.interval_min);
+   const start_time = week.earliest_time.hour * 60 + week.earliest_time.minute;
+   const end_time = week.latest_time.hour * 60 + week.latest_time.minute;
+
+   let num_intervals = (end_time - start_time) / week.interval_minutes;
 
    // import { createEventDispatcher } from "svelte"; 
    
    let gridList = []
+    
+    let count = 0;
 
-   for(let i = 0; i < (num_intervals * 7); i++){
-        gridList.push({
-            value: i,
-            available: false
-        });
+    for(let block = 0; block < num_intervals; block++){
+        for(let day = 0; day < days.length; day++){
+            gridList.push({
+                available: false,
+                id: count,
+
+                col: day, 
+                day: days[day], 
+                
+                row: block,
+
+                hour: Math.floor((start_time + (block * week.interval_minutes)) / 60),
+                minute: (start_time + (block * week.interval_minutes)) % 60
+            });
+            count++;
+        }
+    }
+   let draggedCells = []
+
+   function trackDrag(id){
+        draggedCells = [draggedCells, gridList[id]]; 
    }
 
-   function logClick(id) {
-        console.log(id)
-        gridList[id].available = true;
-        gridList = gridList;
+   function toggleAvailability(id) {
+
+        if(!toggleMode){
+            gridList[id].available = false;
+        } else {
+            gridList[id].available = true;
+        }
+
+        gridList = gridList; // This refresh is important to get the reactivity to work
    }
+
+    $: selecting = false;
+    $: toggleMode = true;
+
+    function handleMouseMove(event){
+        if(selecting){
+            console.log(event.srcElement.id);
+            toggleAvailability(event.srcElement.id);
+        }
+    }
+
+    function handleMouseDown(event){
+        selecting = true;
+        toggleAvailability(event.srcElement.id);
+    }
+
+    function handleMouseUp(event){
+        selecting = false;
+        toggleAvailability(event.srcElement.id);
+    }
 
 </script>
 
-<div class="grid-container">
+<p>{draggedCells} </p>
+<p>
+{#each draggedCells as cell}
+    ({cell.value}, {cell.available}) <br>
+{/each}
+</p>
+
+<div class="grid-container" on:mousemove={handleMouseMove}>
     {#each days as day}
         <div class="grid-item-header">{day}</div>
     {/each}
     {#each gridList as item}
-        {#if item.available}
-            <div class="grid-item-active" id={item.value} 
-             on:mousedown={() => logClick(item.value)} on:focus={() => logClick(item.value)}>
-                {item.value} 
+        {#if item.day == ""}
+            <div class="grid-item-sidebar" id={item.id}> {item.hour}:{item.minute} </div>
+        {:else if item.available}
+            <div class="grid-item-active" id={item.id} 
+                on:mousedown={handleMouseDown} on:mousedown={()=>toggleMode = false}
+                on:mouseup={handleMouseUp}>
             </div>
         {:else}
-            <div class="grid-item-inactive" id={item.value} 
-             on:mousedown={() => logClick(item.value)} on:focus={() => logClick(item.value)}>
-                {item.value} 
+            <div class="grid-item-inactive" id={item.id} 
+                on:mousedown={handleMouseDown} on:mousedown={()=>toggleMode = true}
+                on:mouseup={handleMouseUp}>
             </div>
         {/if}
     {/each}
@@ -47,7 +102,7 @@
 <style>
     .grid-container {
         display: grid;
-        grid-template-columns: auto auto auto auto auto auto auto;
+        grid-template-columns: auto auto auto auto auto auto auto auto;
         column-gap: 0px;
     }
 
@@ -56,6 +111,14 @@
         border: 1px solid rgba(0, 0, 0, 0.8);
         padding: 8px;
         font-size: 20px;
+        text-align: center;
+    }
+    
+    .grid-item-sidebar{
+        background-color: rgba(242, 242, 242, 0.8);
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        padding: 3px;
+        font-size: 10px;
         text-align: center;
     }
 
