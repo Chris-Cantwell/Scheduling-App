cc7a693a784d22017971f4497a1e75702f8dc4ce
Ashleigh Coltman <ashleighcoltman@college.harvard.edu>
2023-03-06 03:59:28 -0500

    Add login style /alert if incorrect pass /navigate

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import '../fonts/fonts.css';
+</script>
+
+<style>
+    body {
+        background-color: #FBFCFF;
+        margin: 0;
+        min-height: 100vh;
+    }
+</style>
+<head>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    />
+</head>
+
+<slot></slot>
